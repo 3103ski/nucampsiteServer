c991ed36be47714e03780671d973aa0c698f7ba4
3103ski <bjastski@gmail.com>
2021-01-15 13:04:35 -0800

    basic setup of express sessions and file store to manage them

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 // Router imports
 var indexRouter = require('./routes/index');
@@ -38,11 +40,22 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(
+	session({
+		name: 'session-id',
+		secret: '12345-67890-09876-54321',
+		saveUninitialized: false,
+		resave: false,
+		store: new FileStore(),
+	})
+);
 
 // this is where we'll add auth
 function auth(req, res, next) {
-	if (!req.signedCookies.user) {
+	console.log(req.session);
+	if (!req.session.user) {
 		const authHeader = req.headers.authorization;
 		if (!authHeader) {
 			const err = new Error('You are not authenticated');
@@ -56,7 +69,7 @@ function auth(req, res, next) {
 		const pass = auth[1];
 
 		if (user === 'admin' && pass === 'password') {
-			res.cookie('user', 'admin', { signed: true });
+			req.session.user = 'admin';
 			return next(); // authorized
 		} else {
 			const err = new Error('You are not authenticated');
@@ -65,7 +78,7 @@ function auth(req, res, next) {
 			return next(err);
 		}
 	} else {
-		if (req.signedCookies.user === 'admin') {
+		if (req.session.user === 'admin') {
 			return next();
 		} else {
 			const err = new Error('You are not authenticated');
