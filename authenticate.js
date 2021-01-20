39fbc5c2887bf2e2441934553a21fc17d7268f88
3103ski <bjastski@gmail.com>
2021-01-19 23:49:05 -0800

    adds OAuth for facebook to nucampsite server

--- authenticate.js
+++ authenticate.js
@@ -1,12 +1,12 @@
+const jwt = require('jsonwebtoken');
+// Passport
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('./models/user');
-
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-
-const jwt = require('jsonwebtoken');
-
+const FacebookTokenStrategy = require('passport-facebook-token');
+// Local
+const User = require('./models/user');
 const config = require('./config.js');
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
@@ -22,6 +22,7 @@ const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
 
+// STRATEGIES
 exports.jwtPassport = passport.use(
 	new JwtStrategy(opts, (jwt_payload, done) => {
 		console.log('JWT payload:', jwt_payload);
@@ -37,6 +38,38 @@ exports.jwtPassport = passport.use(
 	})
 );
 
+exports.facebookPassport = passport.use(
+	new FacebookTokenStrategy(
+		{
+			clientID: config.facebook.clientID,
+			clientSecret: config.facebook.clientSecret,
+		},
+		(accessToken, refreshToken, profile, done) => {
+			User.findOne({ facebookId: profile.id }, (err, user) => {
+				if (err) {
+					return done(err, false);
+				}
+				if (!err && user) {
+					return done(null, user);
+				} else {
+					user = new User({ username: profile.displayName });
+					user.facebookId = profile.id;
+					user.firstname = profile.name.givenName;
+					user.lastname = profile.name.familyName;
+					user.save((err) => {
+						if (err) {
+							return done(err, false);
+						} else {
+							return done(null, user);
+						}
+					});
+				}
+			});
+		}
+	)
+);
+
+// VARIFICATION
 exports.verifyUser = passport.authenticate('jwt', { session: false });
 
 exports.verifyAdmin = (req, res, next) => {
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
 	secretKey: '12345-67890-09876-54321',
 	mongoUrl: 'mongodb://localhost:27017/nucampsite',
+	facebook: {
+		clientID: '1293976254321480',
+		clientSecret: '0459dabbfda0801fe8faa929f09acc1d',
+	},
 };
